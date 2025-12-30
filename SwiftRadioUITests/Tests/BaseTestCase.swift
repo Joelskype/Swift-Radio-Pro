df5224f4cb5b370817c432cda0344d57c2834629
Alexey Riazhev <a.riazhev@magdv.com>
2025-12-30 23:34:20 +0700

    Delete test endpoints. Add and implement BaseTestCase

--- SwiftRadioUITests/Tests/BaseTestCase.swift
+++ SwiftRadioUITests/Tests/BaseTestCase.swift
@@ -7,3 +7,64 @@
 //
 
 import Foundation
+import XCTest
+import Swifter
+
+ class BaseTestCase: XCTestCase {
+    
+    let app = XCUIApplication()
+    var server: HttpServer!
+    var mockStationsData: [[String: Any]] = []
+    var responseStatus: ResponseStatuses = .success
+    
+    
+    override func setUpWithError() throws {
+        
+        continueAfterFailure = false
+        
+        server = HttpServer()
+        mockStationsData = [[
+            
+            "name": "Absolute Country Hits",
+            "streamURL": "http://strm112.1.fm/acountry_mobile_mp3",
+            "imageURL": "station-absolutecountry.png",
+            "desc": "The Music Starts Here",
+            "longDesc": "All your favorite country hits and artists, from Johnny Cash to Taylor Swift, on 1.FM's Absolute Country, playing non-stop crooners and banjos, dance-tunes and fiddles, ballads and harmonicas. Absolute Country focuses on 5th, 6th and 7th generation Country (from the 90s on) but often delves into classic, older tunes as well."
+        ]]
+        
+        server["/stations"] = { [weak self] request in
+            
+            print("🔥 MOCK SERVER: /stations endpoint called!")
+            
+            var successResponce: HttpResponse = .ok(.json(["station": self?.mockStationsData] ?? []))
+            
+            switch self?.responseStatus ?? .success {
+            
+            case .success:
+                return successResponce
+            case .notFound:
+                return .notFound
+            case .serverError:
+                return .internalServerError
+            case .throttling:
+                sleep(1)
+                return successResponce
+            }
+        }
+        
+        try server.start(8080, forceIPv4: true)
+        print("✅ Mock server started on http://localhost:8080")
+    }
+    
+    override func tearDownWithError() throws {
+        
+        server.stop()
+        print("🛑 Mock server stopped")
+    }
+     
+     func setupMockEnvironment() {
+         
+         app.launchEnvironment[TestEnv.url] = EndpointURL.stations
+         app.launchEnvironment[TestEnv.mockServerToggle] = "true"
+     }
+}
--- SwiftRadioUITests/Tests/SwiftRadioUITests.swift
+++ SwiftRadioUITests/Tests/SwiftRadioUITests.swift
@@ -11,272 +11,19 @@ import Swifter
 import Foundation
 
 
-final class SwiftRadioUITests: XCTestCase {
+final class SwiftRadioUITests: BaseTestCase {
     
-    let app = XCUIApplication()
-    var server: HttpServer!
-    var mockStationsData: [[String: Any]] = []
-    var responseStatus: ResponseStatuses = .success
     lazy var stationsScreen = StationsScreen(app: app)
     
-    
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-        
-        server = HttpServer()
-        
-        mockStationsData = [[
-            
-            "name": "Absolute Country Hits",
-            "streamURL": "http://strm112.1.fm/acountry_mobile_mp3",
-            "imageURL": "station-absolutecountry.png",
-            "desc": "The Music Starts Here",
-            "longDesc": "All your favorite country hits and artists, from Johnny Cash to Taylor Swift, on 1.FM's Absolute Country, playing non-stop crooners and banjos, dance-tunes and fiddles, ballads and harmonicas. Absolute Country focuses on 5th, 6th and 7th generation Country (from the 90s on) but often delves into classic, older tunes as well."
-        ]]
-        
-        //        server["/test"] = { request in
-        //            return .ok(.text("Hello from mock server!"))
-        //        }
-        //
-        //        server["/status"] = { request in
-        //            print("Status endpoint called")
-        //            return .ok(.json(["status": "ok", "message": "Server is running"]))
-        //        }
-        //
-        //        server["/echo"] = { request in
-        //
-        //            if let message = request.queryParams.first(where: {$0.0 == "message"})?.1 {
-        //
-        //                return .ok(.json(["echo": message]))
-        //            } else {
-        //
-        //                return .ok(.json(["error": "No message provided"]))
-        //            }
-        //        }
-        //
-        //        server["/data"] = { request in
-        //
-        //            switch request.method {
-        //
-        //            case "GET":
-        //                return .ok(.json(["method": "GET", "description": "Reading data"]))
-        //            case "POST":
-        //                return .ok(.json(["method": "POST", "description": "Creating data"]))
-        //            default:
-        //                return .ok(.json(["error": "method not supported"]))
-        //            }
-        //
-        //        }
-        
-        server["/stations"] = { [weak self] request in
-            
-            print("🔥 MOCK SERVER: /stations endpoint called!")
-            
-            var successResponce: HttpResponse = .ok(.json(["station": self?.mockStationsData] ?? []))
-            
-            switch self?.responseStatus ?? .success {
-            
-            case .success:
-                return successResponce
-            case .notFound:
-                return .notFound
-            case .serverError:
-                return .internalServerError
-            case .throttling:
-                sleep(1)
-                return successResponce
-            }
-        }
-        
-        try server.start(8080, forceIPv4: true)
-        print("✅ Mock server started on http://localhost:8080")
-        
-    }
-    
-    override func tearDownWithError() throws {
-        
-        server.stop()
-        print("🛑 Mock server stopped")
-    }
-    
-    
     func testAppLaunches() {
+        
         app.launch()
-        
-        
         XCTAssertTrue(stationsScreen.isDisplayed())
     }
     
-    //    func testMockServerRespondsTest() throws {
-    //
-    //        let expectation = XCTestExpectation(description: "server responds")
-    //
-    //        let url = URL(string: "http://localhost:8080/test")!
-    //
-    //        let task = URLSession.shared.dataTask(with: url) { data, responce, error in
-    //
-    //            XCTAssertNil(error)
-    //
-    //            XCTAssertNotNil(data)
-    //
-    //            if let data = data,
-    //               let text = String(data: data, encoding: .utf8) {
-    //                        print("📩 Received from server: \(text)")
-    //                        XCTAssertEqual(text, "Hello from mock server!")
-    //                    }
-    //
-    //                    expectation.fulfill()
-    //                }
-    //
-    //                task.resume()
-    //
-    //                // Ждём максимум 5 секунд
-    //                wait(for: [expectation], timeout: 5.0)
-    //        }
-    //
-    //    func testMockServerRespondsStatus() throws {
-    //
-    //        let expectation = XCTestExpectation(description: "server responds")
-    //
-    //        let url = URL(string: "http://localhost:8080/status")!
-    //        let task = URLSession.shared.dataTask(with: url) { data, responce, error in
-    //
-    //            XCTAssertNil(error)
-    //            XCTAssertNotNil(data)
-    //
-    //            if let data = data,
-    //            let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any] {
-    //                print("📩 Received from server: \(json)")
-    //
-    //                XCTAssertEqual(json["status"] as? String, "ok")
-    ////                if let statusValue = json["status"] as? String {
-    ////                    XCTAssertEqual(statusValue, "ok")
-    ////                }
-    //            }
-    //
-    //            expectation.fulfill()
-    //        }
-    //
-    //        task.resume()
-    //
-    //        wait(for: [expectation], timeout: 5.0)
-    //    }
-    //
-    //    func testMockServerRespondsEcho() throws {
-    //
-    //        let expectation = XCTestExpectation(description: "server responds")
-    //
-    //        let url = URL(string: "http://localhost:8080/echo?message=HelloWorld")!
-    //        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-    //
-    //                XCTAssertNil(error)
-    //                XCTAssertNotNil(data)
-    //
-    //            if let data = data,
-    //               let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any] {
-    //
-    //                print("📩 Received from server: \(json)")
-    //
-    //                XCTAssertEqual(json["echo"] as? String, "HelloWorld")
-    //            }
-    //
-    //            expectation.fulfill()
-    //        }
-    //
-    //        task.resume()
-    //
-    //        wait(for: [expectation], timeout: 5.0)
-    //    }
-    //
-    //    func testMockServerRespondsEchoSecond() throws {
-    //
-    //        let expectation = XCTestExpectation(description: "server responds")
-    //
-    //        let url = URL(string: "http://localhost:8080/echo")!
-    //        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-    //
-    //                XCTAssertNil(error)
-    //                XCTAssertNotNil(data)
-    //
-    //            if let data = data,
-    //               let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any] {
-    //
-    //                print("📩 Received from server: \(json)")
-    //
-    //                XCTAssertEqual(json["error"] as? String, "No message provided")
-    //            }
-    //
-    //            expectation.fulfill()
-    //        }
-    //
-    //        task.resume()
-    //
-    //        wait(for: [expectation], timeout: 5.0)
-    //    }
-    //
-    //    func testMockServerGetMethod() throws {
-    //
-    //        let expectation = XCTestExpectation(description: "server responds")
-    //
-    //        let url = URL(string: "http://localhost:8080/data")!
-    //        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-    //
-    //            XCTAssertNil(error)
-    //            XCTAssertNotNil(data)
-    //
-    //            if let data = data,
-    //               let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any] {
-    //
-    //                print("📩 Received from server: \(json)")
-    //
-    //                XCTAssertEqual(json["method"] as? String, "GET")
-    //                XCTAssertEqual(json["description"] as? String, "Reading data")
-    //            }
-    //
-    //            expectation.fulfill()
-    //        }
-    //
-    //        task.resume()
-    //
-    //        wait(for: [expectation], timeout: 5.0)
-    //    }
-    //
-    //    func testMockServerPostMethod() throws {
-    //
-    //        let expectation = XCTestExpectation(description: "server responds")
-    //
-    //        let url = URL(string: "http://localhost:8080/data")!
-    //        var request = URLRequest(url: url)
-    //
-    //        request.httpMethod = "POST"
-    //
-    //        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-    //
-    //            XCTAssertNil(error)
-    //            XCTAssertNotNil(data)
-    //
-    //            if let data = data,
-    //               let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any] {
-    //
-    //                print("📩 Received from server: \(json)")
-    //
-    //                XCTAssertEqual(json["method"] as? String, "POST")
-    //                XCTAssertEqual(json["description"] as? String, "Creating data")
-    //            }
-    //
-    //            expectation.fulfill()
-    //
-    //        }
-    //
-    //        task.resume()
-    //
-    //        wait(for: [expectation], timeout: 5.0)
-    //    }
-    
     func testAppLoadStations() throws {
         
-        app.launchEnvironment[TestEnv.url] = EndpointURL.stations
-        app.launchEnvironment[TestEnv.mockServerToggle] = "true"
+        setupMockEnvironment()
         
         app.launch()
         
@@ -289,8 +36,7 @@ final class SwiftRadioUITests: XCTestCase {
         
         mockStationsData = []
         
-        app.launchEnvironment["MOCK_STATIONS_URL"] = "http://localhost:8080/stations"
-        app.launchEnvironment["USE_MOCK_SERVER"] = "true"
+        setupMockEnvironment()
         
         app.launch()
         
@@ -313,8 +59,7 @@ final class SwiftRadioUITests: XCTestCase {
     func testAppLoadsMultiplyStations() throws {
         
         
-        app.launchEnvironment[TestEnv.url] = EndpointURL.stations
-        app.launchEnvironment[TestEnv.mockServerToggle] = "true"
+        setupMockEnvironment()
         
         mockStationsData = [
             
@@ -348,8 +93,7 @@ final class SwiftRadioUITests: XCTestCase {
     
     func testEnterOnNowPlayingScreen() throws {
         
-        app.launchEnvironment[TestEnv.url] = EndpointURL.stations
-        app.launchEnvironment[TestEnv.mockServerToggle] = "true"
+        setupMockEnvironment()
         
         mockStationsData = [
             
@@ -369,8 +113,7 @@ final class SwiftRadioUITests: XCTestCase {
     
     func testResponceNotFound() {
         
-        app.launchEnvironment[TestEnv.url] = EndpointURL.stations
-        app.launchEnvironment[TestEnv.mockServerToggle] = "true"
+        setupMockEnvironment()
         
         responseStatus = .notFound
         
@@ -381,8 +124,7 @@ final class SwiftRadioUITests: XCTestCase {
     
     func testResponseServerError() {
         
-        app.launchEnvironment[TestEnv.url] = EndpointURL.stations
-        app.launchEnvironment[TestEnv.mockServerToggle] = "true"
+        setupMockEnvironment()
         
         responseStatus = .throttling
         
